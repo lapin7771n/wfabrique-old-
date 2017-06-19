1ea0a15babe097d7bf52b7b559e7e651366506d0
lapin7771n <nikita.lapin.201248@gmail.com>
2017-06-19 17:08:56 +0300

    Delete blocks

--- www/blocks/header.php
+++
@@ -1,6 +0,0 @@
-<div id="header">
-    <img src="images/logo.png" alt="logo">
-    <h1 class="logo">Web-Fabrique</h1>
-    <p>С Web-Fabrique — разработка сайтов
-выходит на новый уровень</p>
-</div>
